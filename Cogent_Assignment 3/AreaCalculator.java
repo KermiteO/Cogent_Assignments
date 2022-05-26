c193cad3d6d9792776b220b802c438d5e6c42630
KermiteO <kermicheo@gmail.com>
2022-05-26 15:12:19 -0700

    Add files via upload

---
+++ Cogent_Assignment 3/AreaCalculator.java
@@ -0,0 +1,13 @@
+package com.cognizant.shapes;
+
+public class AreaCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle rectangle = new Rectangle();
+		
+		System.out.println(rectangle.calculateArea());
+
+	}
+
+}
